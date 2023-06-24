a44340a573ef25f4468715f5bd920a2576c72e8b
NikitaAware <nikita.aware@email.ucr.edu>
2023-06-24 14:46:58 -0500

    Initial Commit

---
+++ FetchRecipe/FetchRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeApp.swift
+//  FetchRecipe
+//
+//  Created by Nikita Aware on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
